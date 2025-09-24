60225063a6d6edaf4aafbeb86ba3b345feae703b
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-09-24 23:21:16 +0300

    added solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,12 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
 
 /**
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction = defaultCompare) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[i], this[j]) > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
+  };
+
+  const defaultCompare = (a, b) => {
+    const strA = String(a);
+    const strB = String(b);
+
+    return strA < strB ? -1 : strA > strB ? 1 : 0;
   };
 }
+
+applyCustomSort();
+
+['b', 'a', 'c'].sort2();
 
 module.exports = applyCustomSort;
