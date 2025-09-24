f8b2324fdbe13948beedbff764372bd9a620f9d9
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-09-25 00:10:38 +0300

    fixed issues

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,28 +5,69 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction = defaultCompare) {
-    for (let i = 0; i < this.length; i++) {
-      for (let j = i + 1; j < this.length; j++) {
-        if (compareFunction(this[i], this[j]) > 0) {
-          [this[i], this[j]] = [this[j], this[i]];
-        }
-      }
-    }
-
-    return this;
-  };
-
   const defaultCompare = (a, b) => {
     const strA = String(a);
     const strB = String(b);
 
     return strA < strB ? -1 : strA > strB ? 1 : 0;
   };
+
+  [].__proto__.sort2 = function (compareFunction = defaultCompare) {
+    if (
+      compareFunction !== undefined &&
+      typeof compareFunction !== 'function'
+    ) {
+      throw new TypeError(
+        'The comparison function must be either a function or undefined',
+      );
+    }
+
+    const undefinedCount = [];
+
+    const nonSparseValues = this.filter((el) => {
+      if (el === undefined) {
+        undefinedCount.push(el);
+
+        return false;
+      }
+
+      return true;
+    });
+
+    let n = nonSparseValues.length;
+    let swapped;
+
+    do {
+      swapped = false;
+
+      for (let i = 0; i < n - 1; i++) {
+        if (compareFunction(nonSparseValues[i], nonSparseValues[i + 1]) > 0) {
+          const temp = nonSparseValues[i];
+
+          nonSparseValues[i] = nonSparseValues[i + 1];
+          nonSparseValues[i + 1] = temp;
+          swapped = true;
+        }
+      }
+      n--;
+    } while (swapped);
+
+    for (let i = 0; i < this.length; i++) {
+      delete this[i];
+    }
+
+    let index = 0;
+
+    for (const value of nonSparseValues) {
+      this[index++] = value;
+    }
+
+    for (const undef of undefinedCount) {
+      this[index++] = undef;
+    }
+
+    return this;
+  };
 }
-
-applyCustomSort();
-
-['b', 'a', 'c'].sort2();
 
 module.exports = applyCustomSort;
